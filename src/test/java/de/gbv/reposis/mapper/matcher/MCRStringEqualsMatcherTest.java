6107aa7416bd6e62ec95c6d4366e6280dd404785
Lennard Golsch <lennard.golsch@icloud.com>
2026-07-06 10:31:09 +0200

    fix: name

--- src/test/java/de/gbv/reposis/mapper/matcher/MCRStringEqualsMatcherTest.java
+++ src/test/java/de/gbv/reposis/mapper/matcher/MCRStringEqualsMatcherTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class MCREqualsStringMatcherTest {
+public class MCRStringEqualsMatcherTest {
 
     @Test
     public void constructorRejectsNullExpectedValue() {
